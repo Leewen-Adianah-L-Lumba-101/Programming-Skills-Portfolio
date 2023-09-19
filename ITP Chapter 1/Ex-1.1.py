e483a834313b4e3417411b8473021fd789065cea
Leewen-Adianah-L-Lumba-101 <144773475+Leewen-Adianah-L-Lumba-101@users.noreply.github.com>
2023-09-19 11:22:37 +0400

    Add files via upload

---
+++ ITP Chapter 1/Ex-1.1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+@author: Leewen Adianah L. Lumba
+"""
+
+"""
+Chapter 1: Getting Started With Python
+"""
+# Exercise 1: Print Strings
+# Write a Python program to print the following string in a specific format.
+# Twinkle, twinkle, little star, How I wonder what you are! 
+# Up above the world so high, Like a diamond in the sky. 
+# Twinkle, twinkle, little star, How I wonder what you are
+
+# The \n command makes statements print on the next line, the \t command only adds a space on the same line.
+print("Twinkle, twinkle, little star,\n\t   How I wonder what you are!\n\t\t\t Up above the world so high,\n\t\t\t Like a diamond in the sky.\nTwinkle, twinkle, little star,\n\t   How I wonder what you are!")
+
