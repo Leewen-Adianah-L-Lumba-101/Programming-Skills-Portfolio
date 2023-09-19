92bff01fcdb013f767a760f6c2d8ec42d222356e
Leewen-Adianah-L-Lumba-101 <144773475+Leewen-Adianah-L-Lumba-101@users.noreply.github.com>
2023-09-19 11:39:59 +0400

    Add files via upload

---
+++ Chapter 1/Ex-1.3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  8 21:54:53 2023
+
+@author: Leewen Adianah L. Lumba
+"""
+
+"""
+Chapter 1: Getting Started With Python
+"""
+# Exercise 3: Print date and time
+# Write a Python program to display the current date and time.
+
+# From the library containing datetime, we can access the details for the current date and time.
+from datetime import datetime
+
+print("The date is:")
+print(datetime.now())
---
+++ Chapter 1/Ex-1.4.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 10:15:54 2023
+
+@author: Leewen Adianah L. Lumba
+"""
+
+"""
+Chapter 1: Getting Started With Python
+"""
+# Exercise 4: Write three strings in different variables and print the output as one string.
+# Write three strings in different variables and print the output as one string. 
+
+a = "Name"
+b = "Age"
+c = "Date"
+
+# The + makes all the different strings of the variables print alongside each other without space.
+# It's now one whole string.
+print(a + b + c)
+
